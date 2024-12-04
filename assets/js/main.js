7ecc4f7802a95c01516712d17fa223e4afe3e3d6
ronaldonyango <ronaldoyw@gmail.com>
2024-12-04 15:34:56 +0300

    Cosmetic improvements

---
+++ assets/js/main.js
@@ -0,0 +1,10 @@
+function getPageLoadTime() {
+    if (window.performance && window.performance.getEntriesByType) {
+        var entries = window.performance.getEntriesByType("navigation");
+        if (entries.length > 0) {
+            var navTiming = entries[0];
+            var pageLoadTime = navTiming.loadEventEnd - navTiming.startTime;
+            return Math.round(((pageLoadTime / 1000) + Number.EPSILON) * 100) / 100;
+        }
+    }
+}
